db7ef09cb9acb6888882db3cf07c3d9421ab39dc
Nelsonlin0321 <nelsonlin0321@outlook.com>
2025-01-05 18:01:06 +0800

    send the email

--- insight-workflow-lambda/src/send_email.py
+++ insight-workflow-lambda/src/send_email.py
@@ -15,10 +15,17 @@ def send_email(mailId: str):
         'X-API-KEY': API_KEY,
         'Content-Type': 'application/json'
     }
-    response = httpx.get(
-        BASE_URL + f"/api/mail/{mailId}", headers=headers, timeout=60)
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        logger.error(f"Failed to send email with the error: {response.text}")
+    for attempt in range(3):  # Retry up to 3 times
+        response = httpx.get(
+            BASE_URL + f"/api/mail/{mailId}", headers=headers, timeout=15)
+
+        if response.status_code == 200:
+            logger.info(response.text)
+            return response.json()
+        else:
+            logger.error(
+                f"Attempt {attempt + 1}: Failed to send email with the error: {response.text}")
+
+    # Log if all attempts fail
+    logger.error("All attempts to send email failed.")
